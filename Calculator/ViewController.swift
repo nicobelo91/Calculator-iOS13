6bddf26c4bdf33b684e381bdd967503d0d4a480b
Nico Cobelo <ingcobelo@gmail.com>
2020-12-04 18:42:35 -0300

    Add isFinishedTypingNumber function to accumulate numbers in the displayLabel

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber: Bool = true
+    
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
     
     }
 
@@ -24,7 +28,16 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
 
 }
